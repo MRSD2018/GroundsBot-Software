5c3edb5da3b1fc10d112914e8dd89fc83b17a226
Henry Chen <zhenrychen@cmu.edu>
2017-11-02 22:28:27 -0400

    working arduino teleop

--- Arduino/grudsby_arduino/grudsby_arduino.ino
+++ Arduino/grudsby_arduino/grudsby_arduino.ino
@@ -5,24 +5,40 @@
 #include "settings.h"
 #include "grudsby_motor.h"
 
-
-ros::NodeHandle nh;
-//ros::Subscriber<grudsby_lowlevel::ArduinoVel> s("/arduino/vel", &velCallback);
-
-
 using namespace grudsby;
 
+
+
+
+void velCallback(const grudsby_lowlevel::ArduinoVel& msg) {
+  writeDirPWM(msg.leftvel, msg.rightvel);
+}
+
+void publishResponse() {
+
+}
+
+ros::NodeHandle nh;
+ros::Subscriber<grudsby_lowlevel::ArduinoVel> vel_sub("/arduino/vel", &velCallback);
 void setup()
 {
+  //set up ros 
   nh.initNode();
-  //nh.subscribe(s);
+  nh.subscribe(vel_sub);
 
-  pinMode(MOTORA1, OUTPUT);
-  pinMode(MOTORB1, OUTPUT);
-  pinMode(MOTORA2, OUTPUT);
-  pinMode(MOTORB2, OUTPUT);
-  pinMode(ENABLE1, OUTPUT);
-  pinMode(ENABLE2, OUTPUT);
+  //set up pins
+  // pinMode(MOTORA1, OUTPUT);
+  // pinMode(MOTORB1, OUTPUT);
+  // pinMode(MOTORA2, OUTPUT);
+  // pinMode(MOTORB2, OUTPUT);
+  // pinMode(ENABLE1, OUTPUT);
+  // pinMode(ENABLE2, OUTPUT);
+
+  pinMode(DIR1, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+
 }
 
 void loop()
