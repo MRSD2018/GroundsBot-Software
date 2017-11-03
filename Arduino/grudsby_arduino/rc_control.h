5e7a00da4edd62d5d6f00d0655fba1d020dae19e
Adam Driscoll <jdriscol@andrew.cmu.edu>
2017-11-02 23:57:21 -0400

    splitting rc_control into cpp and header

---
+++ Arduino/grudsby_arduino/rc_control.h
@@ -0,0 +1,34 @@
+#ifndef RC_CONTROL_H
+#define RC_CONTROL_H
+
+
+#include <Arduino.h>
+#include "SBUS.h"
+
+// a SBUS object, which is on Teensy hardware
+// serial port 1
+SBUS x8r(Serial1);
+
+// channel, fail safe, and lost frames data
+uint16_t channels[16];
+uint8_t failSafe;
+uint16_t lostFrames = 0;
+
+#define THROTTLE channels[2]
+#define TURN channels[0]
+#define KILL_SWITCH channels[5]
+#define CONTROL_MODE channels[4]
+#define REVERSE channels[6]
+
+//void init();
+bool is_killed();
+bool is_autonomous();
+int get_RC_left_motor_velocity();
+int get_RC_right_motor_velocity();
+int get_raw_throttle();
+int get_raw_turn();
+int get_raw_reverse();
+int get_raw_kill();
+int get_raw_mode();
+
+#endif
