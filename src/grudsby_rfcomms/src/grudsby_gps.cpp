ab69fb4901210dcfca62debf80821ea45f91c658
jphaneuf <phaneuf.joe@gmail.com>
2017-11-11 13:19:19 -0500

    uart reads

--- src/grudsby_rfcomms/src/grudsby_gps.cpp
+++ src/grudsby_rfcomms/src/grudsby_gps.cpp
@@ -1,5 +1,6 @@
 //Reference
 //https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
+#include <ros/ros.h>
 #include <errno.h>
 #include <fcntl.h> 
 #include <string.h>
@@ -9,6 +10,15 @@
 #include <stdlib.h>
 #include <grudsby_rfcomms/emlid_reach_gps.h>
 
+////////////////////////////////////////////////////////////////////////////////
+///Outgoing messages////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+grudsby_rfcomms::emlid_reach_gps gps_msg;
+
+
+////////////////////////////////////////////////////////////////////////////////
+///Configure UART TTY///////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 int set_interface_attribs(int fd, int speed)
 {
     struct termios tty;
@@ -44,6 +54,10 @@ int set_interface_attribs(int fd, int speed)
     return 0;
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+///?????????////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void set_mincount(int fd, int mcount)
 {
     struct termios tty;
@@ -61,49 +75,159 @@ void set_mincount(int fd, int mcount)
 }
 
 
-int main()
+////////////////////////////////////////////////////////////////////////////////
+///You spin me right round//////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//int main()
+int main(int argc, char **argv)
 {
-    char *portname = "/dev/ttyUSB0";
+    char *portname = "/dev/ttyACM1";
     int fd;
     int wlen;
+    ros::init(argc, argv, "grudsby_rfcomms");
+    ros::NodeHandle n;
+    ros::Publisher  pub = n.advertise<grudsby_rfcomms::emlid_reach_gps>("grudsby_positioning_system",1000);
+    ros::Rate loop_rate(20);
 
     fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) {
-        printf("Error opening %s: %s\n", portname, strerror(errno));
-        return -1;
-    }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
-    set_interface_attribs(fd, B115200);
+    if (fd < 0) { printf("Error opening %s: %s\n", portname, strerror(errno)); return -1; }
+    set_interface_attribs(fd, B57600); //baudrate 115200, 8 bits, no parity, 1 stop bit
     //set_mincount(fd, 0);                /* set to pure timed read */
 
-    /* simple output */
-    wlen = write(fd, "Hello!\n", 7);
-    if (wlen != 7) {
-        printf("Error from write: %d, %d\n", wlen, errno);
+    while(ros::ok()){
+      unsigned char buf[140];
+      int rdlen;
+      printf("I'm in a loop aaaaan I'm going fast aaaaand \n");
+
+      rdlen = read(fd, buf, sizeof(buf) - 1);
+      if (rdlen > 0) {
+          buf[rdlen] = 0;
+          printf("Read %d: \"%s\"\n", rdlen, buf);
+      } else if (rdlen < 0) {
+          printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+      }
+      
+      /* repeat read to get full message */
+      pub.publish(gps_msg);
+      ros::spinOnce();
+      loop_rate.sleep();
     }
-    tcdrain(fd);    /* delay for output */
-
-
-    /* simple noncanonical input */
-    do {
-        unsigned char buf[80];
-        int rdlen;
-
-        rdlen = read(fd, buf, sizeof(buf) - 1);
-        if (rdlen > 0) {
-#ifdef DISPLAY_STRING
-            buf[rdlen] = 0;
-            printf("Read %d: \"%s\"\n", rdlen, buf);
-#else /* display hex */
-            unsigned char   *p;
-            printf("Read %d:", rdlen);
-            for (p = buf; rdlen-- > 0; p++)
-                printf(" 0x%x", *p);
-            printf("\n");
-#endif
-        } else if (rdlen < 0) {
-            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-        }
-        /* repeat read to get full message */
-    } while (1);
 }
+
+
+/*
+#include <chatbot_node/reply_msg.h>
+#include <message_ui/sent_msg.h>
+#include <counter_node/counter.h>
+#include <arithmetic_node/arithmetic_reply.h>
+#include <string>
+#include <std_msgs/String.h>
+
+using namespace std;
+
+bool new_response = false;
+
+bool process_arithmetic_string(const std::string & str){
+  //////////////////////////////////////////////////////
+  ///Take a message from GUI and check if it is/////////
+  ////arithmetic. Process result and construct reply////
+  ////if so, do nothing otherwise///////////////////////
+  //////////////////////////////////////////////////////
+  std::string::size_type i = 0;
+  int operand1=0;
+  int operand2=0;
+  float result;
+  char operater;
+
+  if(!isdigit(str[0])){
+    ///Enforce first character is numeric///////////////
+    std::cout << "violation: non-integer start" << "\n";
+    return false;
+  }
+
+  do{
+    ///Parse first operand /////////////////////////////
+    if(isdigit(str[i])){
+      operand1 *= 10;
+      operand1 += (int) (str[i] - '0'); 
+    } else{
+      break;
+    }
+    i++;
+  } while(i < str.size());
+
+  ///Parse operator //////////////////////////////////
+  operater = str[i++];
+
+  do{
+    ///Parse second operand ////////////////////////////
+    if(isdigit(str[i])){
+      operand2 *= 10;
+      operand2 += (int) (str[i] - '0'); 
+    } else{
+      std::cout << "violation: end of string nonnumeric" << "\n";
+      return false;
+    }
+    i++;
+  } while(i < str.size());
+
+  ///Compute result based on operands and operator/////
+  if     (operater == '+'){
+    outgoing_reply_message.oper_type = "Add"; 
+    result = operand1 + operand2;
+    outgoing_reply_message.answer = operand1 + operand2;
+  }
+  else if(operater == '-'){
+    outgoing_reply_message.oper_type = "Subtract"; 
+    result = operand1 - operand2;
+    outgoing_reply_message.answer = operand1 - operand2;
+  }
+  else if(operater == '/'){
+    outgoing_reply_message.oper_type = "Divide"; 
+    result = operand1 / (float) operand2;
+    outgoing_reply_message.answer = operand1 / (float) operand2;
+  }
+  else if(operater == '*'){
+    outgoing_reply_message.oper_type = "Multiply"; 
+    result = operand1 * operand2;
+    outgoing_reply_message.answer = operand1 * operand2;
+  }else{
+    std::cout << "violation: invalid operator" << "\n";
+    return 0;
+  }
+  std::cout << "operand1:" << operand1 << " operand2:" << operand2 << " operator:" << operater << " result:" << result << "\n";
+  return 1;
+}
+ 
+
+void message_callback(const message_ui::sent_msg data)
+{
+  outgoing_reply_message.time_received = ros::Time::now().toSec();
+  if( process_arithmetic_string(data.message) ){
+    new_response = true;
+  }
+}
+
+int main(int argc, char **argv) {
+  std::stringstream ss;
+  ros::init(argc, argv, "arithmetic_node");
+  ros::NodeHandle n;
+  ros::Publisher  pub = n.advertise<arithmetic_node::arithmetic_reply>("arithmetic_reply",1000);
+  ros::Subscriber sub = n.subscribe("sent_msg", 1000, message_callback);
+  ros::Rate loop_rate(20);
+
+  while(ros::ok()) {
+    if(new_response){
+      outgoing_reply_message.header.stamp = ros::Time::now();
+      outgoing_reply_message.time_answered = ros::Time::now().toSec();
+      outgoing_reply_message.process_time = outgoing_reply_message.time_answered - outgoing_reply_message.time_received;
+      pub.publish(outgoing_reply_message);
+      new_response = false;
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}*/
