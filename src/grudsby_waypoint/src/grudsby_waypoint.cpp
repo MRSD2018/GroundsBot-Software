d2091bf507398591c2ea561c5bdeec716dc2487a
Adam Driscoll <jdriscol@andrew.cmu.edu>
2017-11-15 11:44:37 -0500

    still no idea what I'm doing but some changes have been made.

--- src/grudsby_waypoint/src/grudsby_waypoint.cpp
+++ src/grudsby_waypoint/src/grudsby_waypoint.cpp
@@ -4,21 +4,54 @@
 #include "nav_msgs/Odometry.h"
 #include "tf2_ros/transform_listener.h"
 #include "geometry_msgs/TransformStamped.h"
+#include "tf2_ros/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <geodesy/wgs84.h>
+#include <geodesy/utm.h>
 #include <sstream>
 
 ros::Publisher waypoint_pub;
 
-void findWaypointCallback(const nav_msgs::Odometry msg) 
+class PoseDrawer
 {
-  //Probably put these in a header
-  tf2_ros::Buffer tfBuffer;
-  tf2_ros::TransformListener tfListener(tfBuffer);
+public:
+  PoseDrawer() :
+    tf2_(buffer_),  target_frame_("turtle1"),
+    tf2_filter_(point_sub_, buffer_, target_frame_, 10, 0)
+  {
+    point_sub_.subscribe(n_, "turtle_point_stamped", 10);
+    tf2_filter_.registerCallback( boost::bind(&PoseDrawer::msgCallback, this, _1) );
+  }
 
-  geometry_msgs::TransformStamped transformStamped;
+  //  Callback to register with tf2_ros::MessageFilter to be called when transforms are available
+  void msgCallback(const geometry_msgs::PointStampedConstPtr& point_ptr) 
+  {
+    geometry_msgs::PointStamped point_out;
+    try 
+    {
+      buffer_.transform(*point_ptr, point_out, target_frame_);
+      
+      ROS_INFO("point of turtle 3 in frame of turtle 1 Position(x:%f y:%f z:%f)\n", 
+             point_out.point.x,
+             point_out.point.y,
+             point_out.point.z);
+    }
+    catch (tf2::TransformException &ex) 
+    {
+      ROS_WARN("Failure %s\n", ex.what()); //Print exception which was caught
+    }
+  }
 
-  //Probably not right...
-  transformStamped = tfBuffer.lookupTransform("tf/odom", "odometry", ros::Time(0));
-}
+private:
+  std::string target_frame_;
+  tf2_ros::Buffer buffer_;
+  tf2_ros::TransformListener tf2_;
+  ros::NodeHandle n_;
+  message_filters::Subscriber<geometry_msgs::PointStamped> point_sub_;
+  tf2_ros::MessageFilter<geometry_msgs::PointStamped> tf2_filter_;
+
+};
 
 int main(int argc, char **argv)
 {
@@ -30,7 +63,7 @@ int main(int argc, char **argv)
 
   ros::Subscriber local_odom_sub;
   //TODO: Fix topic name
-  local_odom_sub = n.subscribe<nav_msgs::Odometry>("/key_vel", 100, findWaypointCallback)
+  local_odom_sub = n.subscribe<nav_msgs::Odometry>("/robot/localization", 100, findWaypointCallback)
 
   ros::Rate loop_rate(10);
 
