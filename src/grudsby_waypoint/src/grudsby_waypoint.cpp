f74d571eb39e2111ab600f9099b34b38f4cbe0d1
Adam Driscoll <jdriscol@andrew.cmu.edu>
2017-11-15 19:32:28 -0500

    super janky but figured out how to convert GPS -> UTM. Still need to do literally everything else

--- src/grudsby_waypoint/src/grudsby_waypoint.cpp
+++ src/grudsby_waypoint/src/grudsby_waypoint.cpp
@@ -7,6 +7,7 @@
 #include "tf2_ros/message_filter.h"
 #include "message_filters/subscriber.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "navsat_conversions.h"
 #include <geodesy/wgs84.h>
 #include <geodesy/utm.h>
 #include <sstream>
@@ -21,9 +22,24 @@ int main(int argc, char **argv)
   
   waypoint_pub = n.advertise<geometry_msgs::PoseStamped>("/goal_waypoint", 1000);
 
-  ros::Subscriber local_odom_sub;
+  //ros::Subscriber local_odom_sub;
   //TODO: Fix topic name
-  local_odom_sub = n.subscribe<nav_msgs::Odometry>("/robot/localization", 100, findWaypointCallback)
+  //local_odom_sub = n.subscribe<nav_msgs::Odometry>("/robot/localization", 100, findWaypointCallback)
+
+  double latitude = 40.44455571433333;
+  double longitude = -79.94060748566666;
+  double easting = 0;
+  double northing= 0;
+  std::string utm_zone_tmp;
+
+  RobotLocalization::NavsatConversions::LLtoUTM(latitude, longitude, northing, easting, utm_zone_tmp);
+
+  //geodesy::UTMPoint::UTMPoint() *converted_coord;
+
+  //geodesy::fromMsg(*wgs_coords, *converted_coord);
+
+  std::cout<<"Easting: "<<easting<<"\t Northing: "<<northing<<std::endl;
+
 
   ros::Rate loop_rate(10);
 
