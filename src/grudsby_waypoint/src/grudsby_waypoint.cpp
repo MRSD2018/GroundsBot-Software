4a0c895e61a17dc778b1220f0c4aa36e05672d67
Adam Driscoll <jdriscol@andrew.cmu.edu>
2017-11-15 21:58:00 -0500

    Skeleton code is up. Need to figure out a way to read in and represent goal waypoints

--- src/grudsby_waypoint/src/grudsby_waypoint.cpp
+++ src/grudsby_waypoint/src/grudsby_waypoint.cpp
@@ -2,17 +2,46 @@
 #include "std_msgs/String.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "nav_msgs/Odometry.h"
-#include "tf2_ros/transform_listener.h"
-#include "geometry_msgs/TransformStamped.h"
-#include "tf2_ros/message_filter.h"
-#include "message_filters/subscriber.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "navsat_conversions.h"
-#include <geodesy/wgs84.h>
-#include <geodesy/utm.h>
+#include <sensor_msgs/NavSatFix.h>
 #include <sstream>
 
 ros::Publisher waypoint_pub;
+
+void findWayPointCallback(const sensor_msgs::NavSatFix& msg)
+{
+  double grudsby_lat = msg->latitude;
+  double grudsby_long = msg->longitude;
+  double grudsby_alt = msg->altitude;
+
+  geometry_msgs::PoseStamped goal;
+  
+  goal.header.stamp = ros:time::now();
+  goal.header.frame_id = "utm";
+
+
+  //PSEUDO CODE
+  //TODO: MAKE THIS REAL!!
+  if ( grudsby_lat == goal_lat && grudsby_long = goal_long )
+  {
+    goal_lat = next_waypoint_lat;
+    goal_long = next_waypoint_long;
+    goal_alt = next_waypoint_alt;
+
+    goal_easting_x = 0;
+    goal_northing_y = 0;
+    std::string utm_zone_tmp;
+
+    RobotLocalization::NavsatConversions::LLtoUTM(goal_lat, goal_long, goal_northing_y, goal_easting_x, utm_zone_tmp);
+
+    goal.pose.position.x = goal_easting_x;
+    goal.pose.position.y = goal_northing_y;
+    goal.pose.position.z = goal_alt;
+  }
+
+  waypoint_pub.plublish(goal)
+}
 
 int main(int argc, char **argv)
 {
@@ -20,11 +49,10 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "waypoint_pub");
   ros::NodeHandle n;
   
-  waypoint_pub = n.advertise<geometry_msgs::PoseStamped>("/goal_waypoint", 1000);
+  waypoint_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
 
-  //ros::Subscriber local_odom_sub;
-  //TODO: Fix topic name
-  //local_odom_sub = n.subscribe<nav_msgs::Odometry>("/robot/localization", 100, findWaypointCallback)
+  ros::Subscriber navsat_sub;
+  navsat_sub = n.subscribe<sensor_msgs::NavSatFix>("gps/fix", 100, findWaypointCallback)
 
   double latitude = 40.44455571433333;
   double longitude = -79.94060748566666;
