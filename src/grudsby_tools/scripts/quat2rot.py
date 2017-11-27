33e6e1768418e2a9f84091aff898d4b4a76d2346
Henry Chen <zhenrychen@cmu.edu>
2017-11-26 22:53:39 -0500

    added quat2rot

---
+++ src/grudsby_tools/scripts/quat2rot.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import rospy
+import roslib
+import tf
+from math import sin, cos, pi
+
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.broadcaster import TransformBroadcaster
+from std_msgs.msg import Int32
+from std_msgs.msg import Float32
+from grudsby_lowlevel.msg import ArduinoResponse
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Vector3
+
+#############################################################################
+class Quat2Rot:
+#############################################################################
+
+    #############################################################################
+    def __init__(self):
+    #############################################################################
+        rospy.init_node("quat2rot")
+        self.nodename = rospy.get_name()
+        rospy.loginfo("-I- %s started" % self.nodename)
+        
+        #### parameters #######
+        self.rate = 100
+        
+       
+        
+        
+        # subscriptions
+        rospy.Subscriber("/imu/data", Imu, self.ImuCallback)
+        
+
+
+        self.rotPub = rospy.Publisher('grudsby/debug/IMUrot', Vector3, queue_size=10)
+       
+    #############################################################################
+    def spin(self):
+    #############################################################################
+        r = rospy.Rate(self.rate)
+        while not rospy.is_shutdown():
+            self.update()
+            r.sleep()
+       
+     
+    #############################################################################
+    def update(self):
+    #############################################################################
+        pass
+            
+            
+
+
+    #############################################################################
+    def ImuCallback(self, msg):
+    #############################################################################
+        quat = (
+		msg.orientation.x,
+		msg.orientation.y,
+		msg.orientation.z,
+		msg.orientation.w)
+	euler = tf.transformations.euler_from_quaternion(quat)
+	
+	rots = Vector3() 
+	rots.x = euler[0]
+	rots.y = euler[1]
+	rots.z = euler[2]
+
+	rotPub.publish(rots)
+	
+
+        
+  
+        
+#############################################################################
+#############################################################################
+
+
+if __name__ == '__main__':
+    """ main """
+    try:
+        quat2rot = Quat2Rot()
+        quat2rot.spin()
+    except rospy.ROSInterruptException:
+        pass
