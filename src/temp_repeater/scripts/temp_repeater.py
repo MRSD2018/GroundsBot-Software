dc8a899c9ccee1fc6d98f60d2109ff88461b3148
Josh Bennett <joshuajbennett@github.com>
2018-01-21 23:50:28 -0500

    Updated the repeater to fix the IMU covariances.

--- src/temp_repeater/scripts/temp_repeater.py
+++ src/temp_repeater/scripts/temp_repeater.py
@@ -5,6 +5,7 @@ import roslib
 roslib.load_manifest('temp_repeater')
 
 from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import Imu
 
 #############################################################################
 class DiffTf:
@@ -20,8 +21,11 @@ class DiffTf:
         self.rate = 100
         self.w = 0.508 #wheelbase len
         # subscriptions
-        rospy.Subscriber("/fix", NavSatFix, self.responseCallback)
+        
         self.gpsPub = rospy.Publisher("/gps/fix", NavSatFix, queue_size=10)
+        self.imuPub = rospy.Publisher("/imu/data_raw_new_covariance", Imu, queue_size=10)
+        rospy.Subscriber("/fix", NavSatFix, self.responseCallback)
+        rospy.Subscriber("/imu/data_raw", Imu, self.imuResponseCallback)
     #############################################################################
     def spin(self):
     #############################################################################
@@ -45,11 +49,37 @@ class DiffTf:
         gps_msg.longitude = msg.longitude
         gps_msg.altitude = msg.altitude
         gps_msg.position_covariance_type = msg.position_covariance_type
-        gps_msg.position_covariance[0] = 0.1
-        gps_msg.position_covariance[4] = 0.1
-        gps_msg.position_covariance[8] = 0.25
+        if msg.status.status == 0: # no rtk
+            gps_msg.position_covariance[0] = 15
+            gps_msg.position_covariance[4] = 15
+            gps_msg.position_covariance[8] = 35
+        else:
+            gps_msg.position_covariance[0] = 0.1
+            gps_msg.position_covariance[4] = 0.1
+            gps_msg.position_covariance[8] = 0.25
         self.gpsPub.publish(gps_msg)
         
+
+    #############################################################################
+    def imuResponseCallback(self, msg):
+    #############################################################################
+        
+        imu_msg = Imu()
+        imu_msg.header = msg.header
+        imu_msg.orientation = msg.orientation
+        imu_msg.angular_velocity = msg.angular_velocity
+        imu_msg.linear_acceleration = msg.linear_acceleration
+        imu_msg.orientation_covariance[0] = 1
+        imu_msg.orientation_covariance[4] = 1
+        imu_msg.orientation_covariance[8] = 1
+        imu_msg.angular_velocity_covariance[0] = 0.5
+        imu_msg.angular_velocity_covariance[4] = 0.5
+        imu_msg.angular_velocity_covariance[8] = 0.5
+        imu_msg.linear_acceleration_covariance[0] = 0.1
+        imu_msg.linear_acceleration_covariance[4] = 0.1
+        imu_msg.linear_acceleration_covariance[8] = 0.1
+        
+        self.imuPub.publish(imu_msg)
         
 #############################################################################
 #############################################################################
