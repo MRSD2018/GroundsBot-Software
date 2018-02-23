50c0aefadbbbaf28da64b45479920ebc1fecd9ce
Adam Driscoll <jdriscol@andrew.cmu.edu>
2018-02-23 09:56:26 -0500

    added gps and imu to moveToModelLibrary script

--- src/grudsby_gazebo/models/moveToModelLibrary.sh
+++ src/grudsby_gazebo/models/moveToModelLibrary.sh
@@ -1,5 +1,7 @@
 cp -r grudsby ~/.gazebo/models
 cp -r grudsby_caster ~/.gazebo/models
 cp -r grudsby_new ~/.gazebo/models
+cp -r grudsby_gps ~/.gazebo/models
+cp -r grudsby_imu ~/.gazebo/models
 
 export GAZEBO_PLUGIN_PATH=`pwd`/devel/lib:$GAZEBO_PLUGIN_PATH
