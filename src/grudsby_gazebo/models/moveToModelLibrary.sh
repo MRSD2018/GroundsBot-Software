2ec3cf68dba78517789beeb80d419c300f94885d
Adam Driscoll <jdriscol@andrew.cmu.edu>
2018-02-24 14:34:26 -0500

    sim grudsby now has a "laser" that's publishing on laser scan_sim

--- src/grudsby_gazebo/models/moveToModelLibrary.sh
+++ src/grudsby_gazebo/models/moveToModelLibrary.sh
@@ -3,5 +3,6 @@ cp -r grudsby_caster ~/.gazebo/models
 cp -r grudsby_new ~/.gazebo/models
 cp -r grudsby_gps ~/.gazebo/models
 cp -r grudsby_imu ~/.gazebo/models
+cp -r grudsby_laser ~/.gazebo/models
 
 export GAZEBO_PLUGIN_PATH=`pwd`/devel/lib:$GAZEBO_PLUGIN_PATH
