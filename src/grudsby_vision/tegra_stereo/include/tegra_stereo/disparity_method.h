738ca0e15f2fd64652cefcd9349e807709846717
Grusby <info@groundsbot.com>
2018-04-18 05:52:38 -0400

    Removed stereo, fixing costmap setup.

--- src/grudsby_vision/tegra_stereo/include/tegra_stereo/disparity_method.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <opencv2/opencv.hpp>
-
-void init_disparity_method(const uint8_t _p1, const uint8_t _p2);
-cv::Mat compute_disparity_method(cv::Mat left, cv::Mat right, float *elapsed_time_ms);
-void finish_disparity_method();
-static void free_memory();
